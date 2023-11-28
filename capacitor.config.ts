5c21abcd6076ea45787db8b600b5f52f2a1753ea
confloresc <con.floresc@duocuc.cl>
2023-11-27 23:40:23 -0300

    otras actualizaciones

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'myApp',
+  appId: 'com.myappsinapi.app',
+  appName: 'myAppSinApi',
   webDir: 'www',
   server: {
     androidScheme: 'https'
