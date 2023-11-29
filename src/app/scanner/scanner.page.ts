3eb38ee4d99c965b46f233c3b6739a4180dea89c
Astrid <ast.gonzalez@duocuc.cl>
2023-11-29 06:58:20 -0300

    actualizacion

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -15,8 +15,6 @@ export class ScannerPage {
   correoElectronico: any;
   nombre: any;
 
-  @ViewChild('video', { static: false }) video?: ElementRef;
-
   constructor(
     private alertController: AlertController,
     private router: Router,
@@ -25,79 +23,38 @@ export class ScannerPage {
 
   
 
-  async checkScannerSupport(): Promise<void> {
-    try {
-      const result = await BarcodeScanner.isSupported();
+  ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
       this.isSupported = result.supported;
-    } catch (error) {
-      console.error('Error al verificar el soporte del escáner:', error);
-      this.isSupported = false;
-      this.presentAlert('Escáner no compatible');
-    }
-  }
-  
-  async startCamera() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: 'environment' },
-      });
-      const videoElement = this.video?.nativeElement;
-      videoElement.srcObject = stream;
-      videoElement.setAttribute('playsinline', 'true');
-      videoElement.play();
-    } catch (error) {
-      console.error('Error al iniciar la cámara:', error);
-    }
+    });
   }
 
   async scan(): Promise<void> {
-    try {
-      const granted = await this.requestPermissions();
-      if (!granted) {
-        this.presentAlert('Por favor, concede permisos de cámara para usar el escáner de códigos de barras.');
-        return;
-      }
-
-      // Llama a la función startCamera() al hacer clic en el botón de escaneo
-      this.startCamera();
-
-      // Opcional: Puedes seguir utilizando BarcodeScanner.scan() si necesitas
-      // realizar alguna acción específica después de escanear un código de barras.
-      const { barcodes } = await BarcodeScanner.scan();
-      this.barcodes.push(...barcodes);
-    } catch (error) {
-      console.error('Error durante el escaneo:', error);
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
     }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
   }
 
   async requestPermissions(): Promise<boolean> {
-    try {
-      const { camera } = await BarcodeScanner.requestPermissions();
-      return camera === 'granted' || camera === 'limited';
-    } catch (error) {
-      console.error('Error al solicitar permisos:', error);
-      return false;
-    }
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
   }
 
-  async presentAlert(message: string): Promise<void> {
+  async presentAlert(): Promise<void> {
     const alert = await this.alertController.create({
-      header: 'Permiso denegado',
-      message,
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
       buttons: ['OK'],
     });
     await alert.present();
   }
 
-  async presentToast(message: string): Promise<void> {
-    const toast = await this.toastController.create({
-      message,
-      duration: 2000,
-      position: 'bottom',
-    });
-    toast.present();
-  }
-
+  
+  
   goToLoginPage() {
     this.router.navigateByUrl('/login');
   }
